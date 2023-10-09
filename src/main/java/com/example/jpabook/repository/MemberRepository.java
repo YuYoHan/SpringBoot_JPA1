5e0b0da79f6a7a97e66b4a7f69e5bf507abfa95d
YuYoHan <zxzz8014@naver.com>
2023-10-09 19:32:00 +0900

    [Study] memberRepository

--- src/main/java/com/example/jpabook/repository/MemberRepository.java
+++ src/main/java/com/example/jpabook/repository/MemberRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
+import java.util.Scanner;
 
 @Repository
 public class MemberRepository {
@@ -22,7 +23,14 @@ public class MemberRepository {
     }
 
     public List<Member> findAll() {
+        // Member에 대한 엔티티 객체에 대해서
         return em.createQuery("select m from  Member  m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(Scanner name) {
+        return  em.createQuery("select  m from Member m where m.userName = :name", Member.class)
+                .setParameter("name", name)
                 .getResultList();
     }
 }
