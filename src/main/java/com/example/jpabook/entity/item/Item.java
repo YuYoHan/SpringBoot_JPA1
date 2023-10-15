54aa42288b0b2da582bce70ac4b305a86414abbe
YuYoHan <zxzz8014@naver.com>
2023-10-15 21:20:25 +0900

    [Study] 상품 엔티티안에 기능 구현

--- src/main/java/com/example/jpabook/entity/item/Item.java
+++ src/main/java/com/example/jpabook/entity/item/Item.java
@@ -1,5 +1,6 @@
 package com.example.jpabook.entity.item;
 
+import com.example.jpabook.exception.NotEnoughStockException;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,4 +27,28 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Catagory> catagories = new ArrayList<>();
+
+    @Builder
+    public Item(Long id, String name, int price, int stockQuantity, List<Catagory> catagories) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+        this.catagories = catagories;
+    }
+
+    // 비즈니스 로직
+    // stock 증가
+    public void addStock(int stockQuantity) {
+        this.stockQuantity += stockQuantity;
+    }
+
+    // stcck 감소
+    public void removeStock(int stockQuantity) {
+        int restStock = this.stockQuantity - stockQuantity;
+        if(restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
