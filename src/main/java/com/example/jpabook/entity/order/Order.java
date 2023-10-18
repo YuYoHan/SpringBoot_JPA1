f1b2cd820573d3a4a632f69c1bec234c26b72bc3
YuYoHan <zxzz8014@naver.com>
2023-10-18 22:35:39 +0900

    [Study] shopping

--- src/main/java/com/example/jpabook/entity/order/Order.java
+++ src/main/java/com/example/jpabook/entity/order/Order.java
@@ -52,6 +52,49 @@ public class Order {
         if(member != null) {
             member.getOrders().add(this);
         }
+    }
 
+    public void addOrderItem(OrderItem orderItem) {
+        this.orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    // 생성 메서드
+    public static Order createOrder(Member member,
+                                    Delivery delivery,
+                                    OrderItem... orderItems2) {
+        ArrayList<OrderItem> arrayList = new ArrayList<>();
+        for(OrderItem orderItem : orderItems2) {
+            arrayList.add(orderItem);
+        }
+        return Order.builder()
+                .member(member)
+                .delivery(delivery)
+                .orderItems(arrayList)
+                .status(OrderStatus.ORDER)
+                .orderDate(LocalDateTime.now())
+                .build();
+    }
+
+    // 비즈니스 로직
+    // 주문 취소
+    public void cancel() {
+        if(delivery.getStatus() == DeliveryStatus.COMP) {
+           throw  new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+        this.status = OrderStatus.CANCEL;
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    // 조회로직
+    // 전체 주문 가격 조회
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
     }
 }
--- src/main/java/com/example/jpabook/entity/order/OrderItem.java
+++ src/main/java/com/example/jpabook/entity/order/OrderItem.java
@@ -26,4 +26,29 @@ public class OrderItem {
 
     private int orderPrice;     // 주문 가격
     private int count;          // 주문 수량
+
+    // 생성 메소드
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        // 주문한 만큼 상품 재고를 까줘야 한다.
+        item.removeStock(count);
+        return orderItem;
+    }
+
+
+    /* 비즈니스 로직 */
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    /* 조회 로직*/
+    // 주문할 때 주문 가격과 수량을 곱해야하기 때문에
+    // 여기서 가져온다.
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
